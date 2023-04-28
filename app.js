af99095532b8e582eba5b73ae8f8cf22fd19a6b8
mhasnain619 <hasnain0893@gmail.com>
2023-04-28 11:19:31 +0500

    not found update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ async function cheackweather(city) {
     location_not_found.style.display = "none";
 
     cityName.innerHTML = `${weather_data.name}`
-    temperature.innerHTML = `${weather_data.main.temp}°C`;
+    temperature.innerHTML = `${Math.round(weather_data.main.temp)}°C`;
     description.innerHTML = `${weather_data.weather[0].description}`;
     humidity.innerHTML = `${weather_data.main.humidity}%`
     wind_speed.innerHTML = `${weather_data.wind.speed}km/H`
